7b7ff8f3d00b22cbf42e7d28ff2b65d9d30fc01c
5522MIKE <18988774300@163.com>
2020-11-29 21:05:24 +0800

    add

--- 最小总和问题.cpp
+++ 最小总和问题.cpp
@@ -12,6 +12,44 @@ using namespace std;
 
 int main()
 {
+    int f[101][101] = {0};
+    int n = 0;
+    int minData = INT_MAX;
+    cout << "输入三角形底边数目：";
+    cin >> n;
+    for (int i = 0; i <= n; i++) //边缘值置无穷大
+    {
+        f[i][0] = INT_MAX;
+        f[0][i] = INT_MAX;
+    }
+    cout << "输入各节点的数：" << endl;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            if (j <= i)
+                cin >> f[i][j];
+            else
+                f[i][j] = INT_MAX;
+        }
+    for (int i = 2; i <= n; i++) //求各节点动态规划过程中的值
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (f[i - 1][j - 1] < f[i - 1][j]) //找相对最小值
+                f[i][j] += f[i - 1][j - 1];
+            else if (f[i - 1][j - 1] > f[i - 1][j]) //找相对最小值
+                f[i][j] += f[i - 1][j];
+            else if (f[i - 1][j - 1] == f[i - 1][j]) //找相对最小值
+                f[i][j] += f[i - 1][j];
+            cout << f[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (f[n][i] < minData)
+            minData = f[n][i];
+    }
+    cout << "到达底边时的最小总和为：" << minData << endl;
     return 0;
 }
-
