04825c0eded48047ed5ecacc7422fcad291a41fd
ming <ming@ming-ubuntu.localdomain>
2021-03-25 01:07:20 -0700

    test
    提交者：MikyMing <18988774300@163.com>

---
+++ template/template.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-03-25 01:01:16
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-03-25 01:02:27
+ * @Description: set
+ * @Analysis: set
+ * @FilePath: /Program/algorithm/template/template.cpp
+ */
+
+#include<bits/stdc++.h>  //万用头文件
+using namespace std;
+
+int main(){
+	cout << "test!";
+}
