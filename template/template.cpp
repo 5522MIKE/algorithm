570668e3e58dfeb1aa0a5eae51f1034f0aec846d
5522MIKE <18988774300@163.com>
2021-03-25 01:15:41 -0700

    test

--- template/template.cpp
+++ template/template.cpp
@@ -2,15 +2,17 @@
  * @Author: MikyMing
  * @Date: 2021-03-25 01:01:16
  * @LastEditors: MikyMing
- * @LastEditTime: 2021-03-25 01:02:27
+ * @LastEditTime: 2021-03-25 01:15:23
  * @Description: set
  * @Analysis: set
- * @FilePath: /Program/algorithm/template/template.cpp
+ * @FilePath: /algorithm/template/template.cpp
  */
 
 #include<bits/stdc++.h>  //万用头文件
+#include<cstdio>
 using namespace std;
 
 int main(){
 	cout << "test!";
+    printf("test2/n");
 }
