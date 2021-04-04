b921514f6aa9a17dddb49750c24c8767081e09f2
5522MIKE <18988774300@163.com>
2021-04-04 09:08:18 -0700

    add

---
+++ gitCommitScription.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/zsh
+###
+ # @Author: MikyMing
+ # @Date: 2021-04-04 08:59:59
+ # @LastEditors: MikyMing
+ # @LastEditTime: 2021-04-04 09:08:06
+ # @Description: set
+ # @Analysis: set
+ # @FilePath: /algorithm/gitCommitScription.sh
+### 
+
+git add .
+
+git commit -m $1
+
+git push
