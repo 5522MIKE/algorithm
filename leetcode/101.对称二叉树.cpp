c4caa9d554bccab79760df758ddb2ceb755983ae
5522MIKE <18988774300@163.com>
2021-04-15 23:29:49 -0700

    leetcode

---
+++ leetcode/101.对称二叉树.cpp
@@ -0,0 +1,41 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-04-15 23:28:15
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-04-15 23:29:38
+ * @Description: set
+ * @Analysis: 双指针，加递归
+ * @FilePath: /algorithm/leetcode/101.对称二叉树.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=101 lang=cpp
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode *p, TreeNode *q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
+// @lc code=end
+
