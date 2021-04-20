35c77ade1d8ce5b9858ae4a2449e4e1d1ed7e34c
5522MIKE <18988774300@163.com>
2021-04-19 23:32:54 -0700

    leetcode

---
+++ leetcode/110.平衡二叉树.cpp
@@ -0,0 +1,47 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-04-19 23:31:22
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-04-19 23:32:46
+ * @Description: set
+ * @Analysis: set
+ * @FilePath: /algorithm/leetcode/110.平衡二叉树.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        } else {
+            return max(height(root->left), height(root->right)) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        } else {
+            return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        }
+    }
+};
+// @lc code=end
+
