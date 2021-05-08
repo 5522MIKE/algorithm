6fd698c53743d89e66ac5202c9a455d957fbc2e1
5522MIKE <18988774300@163.com>
2021-05-08 06:37:17 -0700

    dataStructure

---
+++ dataStructure/testData.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-05-08 06:19:04
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-05-08 06:19:22
+ * @Description: set
+ * @Analysis: set
+ * @FilePath: /algorithm/dataStructure/test.cpp
+ */
+
+#include<bits/stdc++.h>  //万用头文件
+#include<cstdio>
+using namespace std;
+
+int main(){
+	cout << "test!";
+    printf("test2/n");
+}
