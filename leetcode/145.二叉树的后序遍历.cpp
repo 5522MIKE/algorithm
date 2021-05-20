8a7f6c4788b11b16e0199cc8efde26f215669b48
5522MIKE <18988774300@163.com>
2021-05-20 07:53:24 -0700

    leetcode

---
+++ leetcode/145.二叉树的后序遍历.cpp
@@ -0,0 +1,47 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-05-20 07:52:04
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-05-20 07:52:34
+ * @Description: set
+ * @Analysis: set
+ * @FilePath: /algorithm/leetcode/145.二叉树的后序遍历.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=145 lang=cpp
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void postorder(TreeNode *root, vector<int> &res) {
+        if (root == nullptr) {
+            return;
+        }
+        postorder(root->left, res);
+        postorder(root->right, res);
+        res.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        postorder(root, res);
+        return res;
+    }
+};
+// @lc code=end
+
