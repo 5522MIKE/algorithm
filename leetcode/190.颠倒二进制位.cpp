10848a752cdcc3e1f973856390743c29f93eec76
5522MIKE <18988774300@163.com>
2021-05-24 08:09:27 -0700

    leetcode

---
+++ leetcode/190.颠倒二进制位.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-05-24 08:07:07
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-05-24 08:09:16
+ * @Description: set
+ * @Analysis: 无敌的分治。
+ * @FilePath: /algorithm/leetcode/190.颠倒二进制位.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=190 lang=cpp
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+class Solution {
+private:
+    const uint32_t M1 = 0x55555555; // 01010101010101010101010101010101
+    const uint32_t M2 = 0x33333333; // 00110011001100110011001100110011
+    const uint32_t M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    const uint32_t M8 = 0x00ff00ff; // 00000000111111110000000011111111
+
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = n >> 1 & M1 | (n & M1) << 1;
+        n = n >> 2 & M2 | (n & M2) << 2;
+        n = n >> 4 & M4 | (n & M4) << 4;
+        n = n >> 8 & M8 | (n & M8) << 8;
+        return n >> 16 | n << 16;
+    }
+};
+
+// @lc code=end
+
