a199861e6cf0924793f7b36e4e55f381bc3df5a1
5522MIKE <18988774300@163.com>
2021-06-29 01:33:16 -0700

    leetcode

---
+++ leetcode/39.组合总和.cpp
@@ -0,0 +1,45 @@
+/*
+ * @Author: MikyMing
+ * @Date: 2021-06-29 01:28:23
+ * @LastEditors: MikyMing
+ * @LastEditTime: 2021-06-29 01:31:56
+ * @Description: set
+ * @Analysis: set
+ * @FilePath: /algorithm/leetcode/39.组合总和.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=39 lang=cpp
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void dfs(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& combine, int idx) {
+        if (idx == candidates.size()) {
+            return;
+        }
+        if (target == 0) {
+            ans.emplace_back(combine);
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.emplace_back(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> combine;
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+};
+// @lc code=end
+
